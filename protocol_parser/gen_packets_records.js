000f81a22edcdd690976f5709597f392f6ae22e2
Sergey Rublev <narma.nsk@gmail.com>
2012-11-16 04:09:31 +0700

    packet_records, add types and category for gen

--- protocol_parser/gen_packets_records.js
+++ protocol_parser/gen_packets_records.js
@@ -2,7 +2,39 @@
 var erlang = require('./lib/erlang_syntax.js');
 var proto = require('./lib/proto_gen.js');
 
+proto.optimist
+        .boolean(['s', 'c', 't'])
+        .alias('s', 'server')
+        .alias('c', 'client')
+        .alias('t', 'twoway')
+        .describe('s', 'generate server-side packets')
+        .describe('c', 'generate client-side packets')
+        .describe('t', 'generate two-way side packets')
+        ;
+
+var argv = proto.optimist.argv;
+
+var directions = [];
+
+if(argv.s) directions.push('server_to_client');
+if(argv.c) directions.push('client_to_server');
+if(argv.t) directions.push('two_way');
+
+var type_map = {
+  'int': 'integer()',
+  'long': 'integer()',
+  'short': 'integer()',
+  'byte': 'integer()',
+  'float': 'float()',
+  'double': 'float()',
+  'bool': 'boolean()',
+  'string': 'binary()'
+}
+
 function erlang_gen_packet_record(packet) {
+  if(directions.indexOf(packet.direction) < 0) return;
+    
+
     var id = packet['id'];
     var fields = packet['fields'];
     var packet_name = erlang.atomize(packet['name']);
@@ -25,7 +57,17 @@ function erlang_gen_packet_record(packet) {
       var field = fields[f];
       var field_type = erlang.atomize(field['type']);
       var field_name = erlang.atomize(field['name']);
-      acc.push('  ' + field_name);
+
+      var erlang_type = undefined;
+      if(type_map.hasOwnProperty(field_type)) {
+        erlang_type = type_map[field_type];
+      }
+      var s = '';
+
+      if(erlang_type) {
+        s += ' ::' + erlang_type;
+      }
+      acc.push('  ' + field_name + s);
     }
     console.log(acc.join(',\n'));
     console.log('}).');
